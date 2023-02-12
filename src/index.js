71fc38162bf9cb8d1a241f04a75a2c77b3abd576
Anastasiya Klevko <klevko.anastasiya@gmail.com>
2023-02-13 02:06:16 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix){
+    return [];
+  }
+  let newMatrix = matrix;
+  let reverseMatrix = [];  
+  newMatrix.forEach(function(item, index){
+    if (index % 2 !== 0) {
+      item = item.reverse();
+    }
+    reverseMatrix = [...reverseMatrix,...item];
+  })
+  return reverseMatrix;
 }
